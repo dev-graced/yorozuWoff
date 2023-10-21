4f76e1f8560d19800d34381999568e83396e6a87
dev-graced <110510211+dev-graced@users.noreply.github.com>
2023-10-21 21:46:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+//const axios = require('axios');
 
 const CLIENT_ID = "479289013154-2l1m68f7pc3dp21o1voscge1h82cqeqe.apps.googleusercontent.com"
 const CLIENT_SECRET = "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"
@@ -31,7 +31,7 @@ async function main() {
     const { result } = response.data.response;
     console.log(result);
 
-    document.getElementById('domainIdField').textContent = result;
+    //document.getElementById('domainIdField').textContent = result;
   } catch (e) {
     throw e;
   }
@@ -57,5 +57,9 @@ async function getAccessToken() {
   return accessToken;
 }
 
-main()
+async function test() {
+  document.getElementById('domainIdField').textContent = "OKKKKK?";
+}
+
+test()
 
