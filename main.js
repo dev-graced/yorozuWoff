7a211d07fdec5562ea3b08a421cf4ce4afc4f181
dev-graced <110510211+dev-graced@users.noreply.github.com>
2023-10-21 15:18:08 +0000

    test

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 //const axios = require('axios');
+import * as https from "https";
 
 const CLIENT_ID = "479289013154-2l1m68f7pc3dp21o1voscge1h82cqeqe.apps.googleusercontent.com"
 const CLIENT_SECRET = "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"
-const REFRESH_TOKEN = "1//040_FdivnbfKACgYIARAAGAQSNwF-L9IrvrI73MsPi93Riz3nZSvWl7Yk8IIN_Qz2iCJZXKacfX5mcLoMWN3xZdNg6WHJnVMcV6U"
+const REFRESH_TOKEN = "1//04vn2T0NTYgeICgYIARAAGAQSNwF-L9IruSUiN7scF9ODyB8naxyb1mxjQOJy6OtQ9B8aCsqQHC_yulFDEyhikpwEJgtqJhxCR_o"
 const SCRIPT_ID = "1b4ljgunjX9B-Yql8C5fiACUziGNrIe3tGwSD77PUMdDoCdZyc9sf9yiO"
+const TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token"
 /**
  * メイン関数
  * @return string
  */
 async function main() {
-  document.getElementById('domainIdField').textContent = "OKKKK?";
-  //try {
+  try {
+    const url = `https://script.googleapis.com/v1/scripts/${SCRIPT_ID}:run`;
     // アクセストークンを取得する
-    //const accessToken = await getAccessToken();
-     //document.getElementById('domainIdField').textContent = "OK?";
-  //   // GASを実行
-  //   const url = `https://script.googleapis.com/v1/scripts/${SCRIPT_ID}:run`;
+   
+    const accessToken = await getAccessToken();
+    //document.getElementById('domainIdField').textContent = "YKK?";
+     // GASを実行
+     
   //   const headers = {
   //     Authorization: `Bearer ${accessToken}`,
   //   };
@@ -33,29 +36,42 @@ async function main() {
   //   console.log(result);
 
   //   //document.getElementById('domainIdField').textContent = result;
-  // } catch (e) {
-  //   throw e;
-  //}
+  } catch (e) {
+     throw e;
+  } 
 }
 
 /**
  * アクセストークンを取得する関数
  * @return string
  */
-async function getAccessToken() {
-  const payload = {
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
-    refresh_token: REFRESH_TOKEN,
-    grant_type: 'refresh_token',
-  };
-  // リクエスト
-  const response = await axios.post(TOKEN_URL, payload);
-  if (response.status !== 200 || !response.data) {
-    throw new Error('Failed to get access token.');
-  }
-  const accessToken = response.data.access_token;
-  return accessToken;
+async function getAccessToken(){
+  
+  return new Promise(function (resolve) {
+    const data = JSON.stringify({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      refresh_token: REFRESH_TOKEN,
+      grant_type: 'refresh_token',
+    });
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    
+    const reque = https.request(TOKEN_URL, options, (res)=>{
+      // console.log(res)
+      res.on('data', (chunk) => {
+        let access_token = JSON.parse(chunk.toString())
+        resolve(access_token.access_token)
+      })
+    })
+    document.getElementById('domainIdField').textContent = "YYKK?";
+    reque.write(data)
+    reque.end()
+  })
 }
 
 async function test() {
