e667684be7056b68b4a35098b7c30f91b9ee5861
dev-graced <110510211+dev-graced@users.noreply.github.com>
2023-10-22 13:53:54 +0000

    refresh token から access token を取得できるようにした

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
-//const axios = require('axios');
-//import * as https from "https";
-
 const CLIENT_ID = "479289013154-2l1m68f7pc3dp21o1voscge1h82cqeqe.apps.googleusercontent.com"
 const CLIENT_SECRET = "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"
-const REFRESH_TOKEN = "1//04vn2T0NTYgeICgYIARAAGAQSNwF-L9IruSUiN7scF9ODyB8naxyb1mxjQOJy6OtQ9B8aCsqQHC_yulFDEyhikpwEJgtqJhxCR_o"
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
+const AUTHORIZATION_CODE = '4/0AfJohXkQA7gUkvNjAhHN9OQMA8HYYN9vLF_lEnPj6IPpS4HImNMnYrhPFvNXUS3C4lcNBg'; // リダイレクトURIから取得した認証コード
+const REFRESH_TOKEN = "1//04hTkjHAHV8LbCgYIARAAGAQSNwF-L9IrvfsiVGj-YKMSOic16EMTIgNTsTZvV2_6K3z37x41O-IKSxgeSp_udijrjFvZ3p-Js6Y"
+const TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
 const SCRIPT_ID = "AKfycbwGhfPrq3DeBK60vtPsBa5EIDXX4sGMk4YgH6dSYgyoZD_m0DKxhp4dqStpMYrre7Vo3g"
-const TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token"
-/**
- * メイン関数
- * @return string
- */
+
 async function main() {
   try {
     // アクセストークンを取得する
-    //const accessToken = await getAccessToken();
-    //document.getElementById('domainIdField').textContent = "YKK?";
-     // GASを実行
+    let accessTokenRes = await getAccessToken();
+    let accessToken = accessTokenRes.access_token;
+    document.getElementById('accessTokenField').textContent = accessToken;
      
-     const headers = {
-  //     Authorization: `Bearer ${accessToken}`,
-         Authorization: `Bearer "ya29.a0AfB_byBrmgsQva0wNPjOYUSrRhxnsVzcFQYiMyCcqR17FlLQGSOoJPxdGxWR8wJco7cJ1Y_G1AuJ_6eT8WuOr5aL3Vg0slDyFe3DO5y6ctaz8JL6jIlfCCUhPTjPlMh5zbIkPQXMAncvlUQIVGtcTx0OPF6hoHZB9rHFaCgYKAawSARISFQGOcNnCLRet0uudwDvgyZYmuLrpCw0171"`,
-     };
-
+    //// yorozuAPI を叩く
+    // リクエストオプションを設定
+    const headers = {
+      authorization: `Bearer ${accessToken}`
+    };
     const payload = {
       function: 'helloworld',
-      parameters: ['OKKKKKKKKKK']
+      parameters: ['こんばんはdesu']
     };
-
-     // リクエストオプションを設定
     const requestOptions = {
       method: "POST",
       headers: headers,
@@ -36,34 +30,12 @@ async function main() {
 
      // リクエスト
      const url = `https://script.googleapis.com/v1/scripts/${SCRIPT_ID}:run`;
-     //const instance = axios.create({ headers });
-     //const response = await instance.post(url, payload);
+     let apiResponse = await sendPostRequest(url,requestOptions);
+     let text = apiResponse.response.result
+     //console.log(response);
 
-     // Fetch APIを使用してPOSTリクエストを送信
-    fetch(url, requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTPエラー! ステータスコード: ${response.status}`);
-      }
-      return response.json(); // レスポンスをJSONとして解釈
-    })
-    .then(data => {
-      //console.log("POSTリクエストのレスポンスデータ:", data);
-      // ここでデータを処理または表
-      let apiResponse = data.response.result;
-      document.getElementById('domainIdField').textContent = apiResponse;
-      console.log("APIレスポンス",apiResponse);
-    })
-    .catch(error => {
-      console.error("エラー:", error);
-    });
-
-     //document.getElementById('domainIdField').textContent = "B";
-    //  if (response.status !== 200 || !response.data || response.data.error) {
-    //      throw new Error('Failed to run google apps script.');
-    //  }
-    //  const { result } = response.data.response;
-    //  console.log(result);
+     //API の戻り値をテーブルに記入
+     document.getElementById('domainIdField').textContent = text;
 
   } catch (e) {
      throw e;
@@ -71,36 +43,49 @@ async function main() {
 }
 
 /**
- * アクセストークンを取得する関数
- * @return string
+ * refresh_token or authorization_code から google API のアクセストークンを取得する関数
  */
-async function getAccessToken(){
-  return new Promise(function (resolve) {
+ async function getAccessToken(){
+   return new Promise(function (resolve) {
     
-    const data = JSON.stringify({
+    const data = {
+      //code: AUTHORIZATION_CODE,
       client_id: CLIENT_ID,
       client_secret: CLIENT_SECRET,
+      redirect_uri: REDIRECT_URI,
       refresh_token: REFRESH_TOKEN,
-      grant_type: 'refresh_token',
-    });
+      grant_type: 'refresh_token'
+      //grant_type: 'authorization_code'
+    };
     
     const options = {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
+      body: JSON.stringify(data)
     };
-    
-    // const reque = https.request(TOKEN_URL, options, (res)=>{
-    //   // console.log(res)
-    //   res.on('data', (chunk) => {
-    //     let access_token = JSON.parse(chunk.toString())
-    //     resolve(access_token.access_token)
-    //   })
-    // })
-    document.getElementById('domainIdField').textContent = "YYKKK?";
-    reque.write(data)
-    reque.end()
+
+    let response = sendPostRequest(TOKEN_URL,options);
+    resolve(response);
+  })
+ }
+
+// POSTリクエストを送信する関数
+function sendPostRequest(url,requestOptions) {
+  return new Promise((resolve) => {
+    // Fetch APIを使用してPOSTリクエストを送信
+    fetch(url,requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTPエラー! ステータスコード: ${response.status}`);
+      }
+      resolve(response.json());
+    })
+    .catch(error => {
+      result = error;
+      console.error("エラー:", error);
+    });
   })
 }
 
