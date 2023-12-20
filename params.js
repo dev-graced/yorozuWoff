d91036cdf2cc2ed9157c21e9c1bbb594e69e14b9
dev-graced <110510211+dev-graced@users.noreply.github.com>
2023-12-20 13:51:01 +0000

    よろず相談所HPの開き方の説明動画を追加

--- params.js
+++ params.js
@@ -4,12 +4,12 @@ export const CLIENT_ID = "479289013154-2l1m68f7pc3dp21o1voscge1h82cqeqe.apps.goo
 export const CLIENT_SECRET = "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"
 export const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
 export const AUTHORIZATION_CODE = '4/0AfJohXlNpoH2_0uQnv6pE4eZ2OYUMxxpNITVVKEa4A-MHv6Wab1EbdjIOANb_QlawDQE_w'; // リダイレクトURIから取得した認証コード
-export const REFRESH_TOKEN = "1//04kMo9sRjotXNCgYIARAAGAQSNwF-L9IrMKjyR1Z7rSf8-0PRTM04cdKM2c7hMtx0YjPKf2-cqdWHWOsZ_e1Vf34pIx4-r9kEcbw"
+export const REFRESH_TOKEN = "1//047ZZlEmZ9jfICgYIARAAGAQSNwF-L9IrnOyKAS4WNvxLKQWXjwUzqq3b51Xxs3nWxRCY5s1qmibG6_qbO3v4U2UFxX1nhQsP-GU"
 export const TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
 //export const url = `https://script.googleapis.com/v1/scripts/${SCRIPT_ID}:run`
 
 // 実運用モードと開発モードの切り替え設定
-export const SCRIPT_ID = "AKfycbwGhfPrq3DeBK60vtPsBa5EIDXX4sGMk4YgH6dSYgyoZD_m0DKxhp4dqStpMYrre7Vo3g" //本番用
-//export const SCRIPT_ID = "AKfycbz2d8acJnBkhwaHrplkaBCAqe4FqMiBong648t6FAAQ" //テスト用
-export const DEBUG_FLAG = 0 // 0: operation mode  1: debug mode
-export const ACCESS_FLAG = 0 // 0: access to yorozu app only from LINE WORKS  1: access from web brower is allowed
+//export const SCRIPT_ID = "AKfycbwGhfPrq3DeBK60vtPsBa5EIDXX4sGMk4YgH6dSYgyoZD_m0DKxhp4dqStpMYrre7Vo3g" //本番用
+export const SCRIPT_ID = "AKfycbz2d8acJnBkhwaHrplkaBCAqe4FqMiBong648t6FAAQ" //テスト用
+export const DEBUG_FLAG = 1 // 0: operation mode  1: debug mode
+export const ACCESS_FLAG = 1 // 0: access to yorozu app only from LINE WORKS  1: access from web brower is allowed
