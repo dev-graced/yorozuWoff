83865d42caa6c4b785400681fb8f40ffef053f81
dev-graced <110510211+dev-graced@users.noreply.github.com>
2024-01-11 11:25:28 +0000

    do

--- params.js
+++ params.js
@@ -4,12 +4,12 @@ export const CLIENT_ID = "479289013154-2l1m68f7pc3dp21o1voscge1h82cqeqe.apps.goo
 export const CLIENT_SECRET = "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"
 export const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
 export const AUTHORIZATION_CODE = '4/0AfJohXlNpoH2_0uQnv6pE4eZ2OYUMxxpNITVVKEa4A-MHv6Wab1EbdjIOANb_QlawDQE_w'; // リダイレクトURIから取得した認証コード
-export const REFRESH_TOKEN = "1//04sKkp8-S-zYmCgYIARAAGAQSNwF-L9IrhQOHh3JCM8Rvw3rL4tSykmGJ0qmBqX8-lTm8XhEZMdzzKT7gW68WEiv62OKu_5Y5DF8"
+export const REFRESH_TOKEN = "1//04CdWVlTCwr5fCgYIARAAGAQSNwF-L9IrOI339zzlwATI7aRZpq3GIuK70gCyr4CP3irwQoXce8Ub8LjTEaeymaBPW5upTYlDwcI"
 export const TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
 //export const url = `https://script.googleapis.com/v1/scripts/${SCRIPT_ID}:run`
 
 // 実運用モードと開発モードの切り替え設定
 export const SCRIPT_ID = "AKfycbwGhfPrq3DeBK60vtPsBa5EIDXX4sGMk4YgH6dSYgyoZD_m0DKxhp4dqStpMYrre7Vo3g" //本番用
 //export const SCRIPT_ID = "AKfycbz2d8acJnBkhwaHrplkaBCAqe4FqMiBong648t6FAAQ" //テスト用
-export const DEBUG_FLAG = 0 // 0: operation mode  1: debug mode
-export const ACCESS_FLAG = 0 // 0: access to yorozu app only from LINE WORKS  1: access from web brower is allowed
+export const DEBUG_FLAG = 1 // 0: operation mode  1: debug mode
+export const ACCESS_FLAG = 1 // 0: access to yorozu app only from LINE WORKS  1: access from web brower is allowed
