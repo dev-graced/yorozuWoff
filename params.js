e4d505b5c18673b30fa71ba2add71d4e471ef8ba
dev-graced <110510211+dev-graced@users.noreply.github.com>
2024-04-19 14:01:36 +0000

    refresh token update

--- params.js
+++ params.js
@@ -4,7 +4,7 @@ export const CLIENT_ID = "479289013154-2l1m68f7pc3dp21o1voscge1h82cqeqe.apps.goo
 export const CLIENT_SECRET = "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"
 export const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
 export const AUTHORIZATION_CODE = '4/0AfJohXlNpoH2_0uQnv6pE4eZ2OYUMxxpNITVVKEa4A-MHv6Wab1EbdjIOANb_QlawDQE_w'; // リダイレクトURIから取得した認証コード
-export const REFRESH_TOKEN = "1//043MHW_EZnDpQCgYIARAAGAQSNwF-L9IrPsPCW1yknZhCJVmoqyvDQkMEWMCDX5PZebeE3i_Wy4Q2-RFfnJ9wHUAa_6Cyqntg_Xk"; //2024/03/16 更新
+export const REFRESH_TOKEN = "1//04vP9gAT9lw2wCgYIARAAGAQSNwF-L9Ir_hy5P86bO-RsnyHn5DdzxgV7h88-Z9y8cqfdKVBqK_YjtbxQY8xKOMr8eQ9-UB8rqLM"; //2024/03/16 更新
 export const TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
 //export const url = `https://script.googleapis.com/v1/scripts/${SCRIPT_ID}:run`
 
